0882be298884bb7996190df2b2e9e9d49b2c898f
sajidj <sajidjaber@gmail.com>
2013-11-25 18:41:03 -0600

    fix message portion

--- index.py
+++ index.py
@@ -3,15 +3,12 @@ class name(wx.Frame):
     def __init__(self,parent,id):
         wx.Frame.__init__(self,parent,id,'Sajid first program',size=(300,200))
         panel=wx.Panel(self)
-        box=wx.MessageDialog(None,'Testing, is this program working?',  'Git',wx.YES_NO)
+        box=wx.MessageDialog(None,'Testing, is this program working?', 'Git',wx.YES_NO)
         if box.ShowModal() == wx.ID_YES:
-            box.Destroy()
             print("this is the write answer")
-            
-        if box.ShowModal() == wx.ID_NO:
-            box.Destroy()
+        elif box.ShowModal() == wx.ID_NO:
             print ("Wrong answer try again")
-            
+            print box
 if __name__=='__main__':
     app=wx.PySimpleApp()
     frame=name(parent=None,id=-1)
